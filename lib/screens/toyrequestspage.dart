6bcae4199286135d7e4fe3d157c63242a28ca23d
Leedje Bocio Amancio <545638@student.fontys.nl>
2025-10-03 11:58:29 +0200

    layered buttons in the requests overview

--- lib/screens/toyrequestspage.dart
+++ lib/screens/toyrequestspage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:playpool_flutter/models/toyDTO.dart';
 import 'package:playpool_flutter/viewmodels/appState.dart';
 import 'package:playpool_flutter/widgets/toyCard.dart';
 import 'package:provider/provider.dart';
@@ -12,6 +11,7 @@ class ToyRequestsPage extends StatefulWidget {
 
 class _ToyRequestsPageState extends State<ToyRequestsPage> {
   void onPressed() {}
+
   @override
   Widget build(BuildContext context) {
     var appContext = context.watch<AppState>();
@@ -20,41 +20,49 @@ class _ToyRequestsPageState extends State<ToyRequestsPage> {
     return Container(
       color: const Color.fromARGB(255, 219, 219, 219),
       child: ListView(
-        children: [
-          ...requestedToys.map((toy) => ToyCard(toy: toy)),
-          Row(
-            spacing: 10,
+        children: requestedToys.map((toy) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              OutlinedButton(
-                onPressed: () {
-                  appContext.declineRequest("ToyID 1");
-                  //Display toy removed notification
-                },
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  side: BorderSide(width: 2, color: Color(0xFFFFC943)),
-                ),
-                child: Text(
-                  "Decline Request",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              FilledButton(
-                onPressed: () {
-                  //Accept request: Forward to confirmation page and push the toy object into the view
-                  context.go('/toycheckout');
-                },
-                style: FilledButton.styleFrom(
-                  backgroundColor: Color(0xFFFFC943),
-                ),
-                child: Text(
-                  "Accept Request",
-                  style: TextStyle(color: Colors.black),
-                ),
+              ToyCard(toy: toy),
+              Row(
+                spacing: 10,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      appContext.declineRequest(toy.id);
+                      // Display toy removed notification
+                    },
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      side: const BorderSide(
+                        width: 2,
+                        color: Color(0xFFFFC943),
+                      ),
+                    ),
+                    child: const Text(
+                      "Decline Request",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      // Accept request: Forward to confirmation page and push the toy object into the view
+                      context.go('/toycheckout');
+                    },
+                    style: FilledButton.styleFrom(
+                      backgroundColor: const Color(0xFFFFC943),
+                    ),
+                    child: const Text(
+                      "Accept Request",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
               ),
             ],
-          ),
-        ],
+          );
+        }).toList(),
       ),
     );
   }
